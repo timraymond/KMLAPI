15984bf1617c98c915d667cde3c2e520673b35a1
Sam H <samhuntress@gmail.com>
2012-05-31 14:42:29 -0400

    JUnit stuff added

---
+++ test/MapControllerTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MapControllerTest {
+
+	@Test
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoDeleteHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateShip() {
+		fail("Not yet implemented");
+	}
+
+}
